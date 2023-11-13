7253a728fbba99984e771debcc36eb5207d71784
Austin Hardy <auha2401@colorado.edu>
2023-11-13 11:45:30 -0700

    table

---
+++ src/init_data/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students(
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    level VARCHAR(15) NOT NULL,
+    ski_or_board VARCHAR(30) NOT NULL,
+    degree VARCHAR(15) NOT NULL
+);
