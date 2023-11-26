04162f2d7f4655175c059b5b0eb7f7417151f1e8
anushaven <anve2046@colorado.edu>
2023-11-25 20:12:11 -0700

    Discover page mostly complete

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,25 +1,13 @@
 CREATE TABLE students(
+    username VARCHAR(50) PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    email VARCHAR(200) NOT NULL
-);
-
-CREATE TABLE mountains(
-    resort_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    skill_level SMALLINT NOT NULL,
-    preference VARCHAR(15) NOT NULL,
-    mode_trans VARCHAR(30) NOT NULL
-    
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE tags(
-    level SMALLINT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    location VARCHAR(50) NOT NULL,
-    ski_or_board VARCHAR(30) NOT NULL
-);
-
-CREATE TABLE users(
-    username VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(60) NOT NULL
+    ski_or_board VARCHAR(30) SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL, -- foreign key from students table
+    mtn_name VARCHAR(50) NOT NULL,
+    skill_level SMALLINT NOT NULL, 
 );
